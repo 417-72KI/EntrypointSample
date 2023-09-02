eacbe3cf4bdb43ffa6cf85de3eebf36f99ad5f24
417-72KI <417.72ki@gmail.com>
2023-09-03 02:37:49 +0900

    setup `SceneDelegate` programmatically

--- Swift-UIKit/EntrypointSample-Swift-UIKit/AppDelegate.swift
+++ Swift-UIKit/EntrypointSample-Swift-UIKit/AppDelegate.swift
@@ -16,9 +16,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let conf = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        conf.delegateClass = SceneDelegate.self
+        return conf
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
