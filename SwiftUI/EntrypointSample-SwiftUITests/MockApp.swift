1f1945cb311b10d4a0993366458aa7c3958ade20
417-72KI <417.72ki@gmail.com>
2023-09-03 02:17:09 +0900

    add `App` mock

---
+++ SwiftUI/EntrypointSample-SwiftUITests/MockApp.swift
@@ -0,0 +1,21 @@
+//
+//  MockApp.swift
+//  EntrypointSample-SwiftUITests
+//
+//  Created by 417.72KI on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct MockApp: App {
+    init() {
+        print("This is MockApp.")
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
