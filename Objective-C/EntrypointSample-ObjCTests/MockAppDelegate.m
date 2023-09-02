b332068a3c5f0c8d555090b03577c73ef6435925
417-72KI <417.72ki@gmail.com>
2023-09-03 02:11:09 +0900

    add `AppDelegate` mock

---
+++ Objective-C/EntrypointSample-ObjCTests/MockAppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  MockAppDelegate.m
+//  EntrypointSample-ObjCTests
+//
+//  Created by 417.72KI on 2023/09/03.
+//
+
+#import "MockAppDelegate.h"
+
+@implementation MockAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"This is MockAppDelegate");
+    return YES;
+}
+
+@end
